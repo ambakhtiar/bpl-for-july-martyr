6d831202ae203220093e23e1362b407ada9b382e
ambakhtiar <ambakhtiar88@gmail.com>
2025-05-24 12:56:41 +0600

    setup environment

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <h1 className='text-5xl font-bold'>bpl for july revolation</h1>
+        </>
+    )
+}
+
+export default App
